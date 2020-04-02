2553f7e940f3d47ba530bf4483545d660ca76cf3
naman2710 <naman.verma1985@gmail.com>
2020-04-02 20:11:57 +0530

    commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,21 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+		int i = 1;
+		i=29;
+		i=287;
+		i=22455;
+		i=243;
+		i=25432;
+		i=221;
+		i=232;
+		i=251;
+		i=2224;
+		i=2223;
+		i=223;
+		i=23;
+		i=23;
+		i=22;
     }
 
     private final String getMessage() {
