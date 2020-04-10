52f63ebe03aebac83c1bef2385004776554fd64d
naman2710 <naman.verma1985@gmail.com>
2020-04-10 09:49:03 +0530

    commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,31 +13,10 @@ public class App
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
 		int l=11;
-		l=11463;
-		l=11231;
-		l=11123;
-		l=11124;
-		l=1155;
-		l=1157;
-		l=1167;
-		l=1125;
-		l=11436547;
-		l=1143;
-		l=1134757;
-		l=11346577;	
-		l=1134675;
-		l=115686;
-		l=1145;
-		l=1127;
-		l=11327;
-		l=118546;
-		l=1176;
-		l=18456;
-		l=16346;
-		l=1534;
-		l=1435;
-		l=1234;			
-		
+		m=10;
+		m=100;
+		m=100;
+		m=100;
     }
 
     private final String getMessage() {
