bb63336f9882c2bb29e80bddd47b48c57c3999f3
naman2710 <naman.verma1985@gmail.com>
2020-04-11 14:35:50 +0530

    smdf

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -13,48 +13,17 @@ import static org.junit.Assert.*;
 public class AppTest
 {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
+    App app = new App();
 
     @Test
-    public void testAppConstructor() {
-        try {
-            new App();
-			int i = 0;
-			i = 2;
-			i = 25;
-			i = 24;
-			i = 22;
-			i = 11;
-			i = 8;
-			i = 7;
-			i = 6;
-			i = 5;
-			i = 4;
-			i = 3;
-        } catch (Exception e) {
-            fail("Construction failed.");
-        }
-    }
-
-    @Test
-    public void testAppMain()
+    public void testApp()
     {
-        App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
-        }
+        assertEquals("Hello world", app.greet("world"));
     }
 
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
+    @Test
+    public void testTrue()
+    {
+        assertTrue( true );
     }
-
 }
